7de9bb310dc329d97e68db837b9e59124a3dbc66
GouthamSurendran <gouthu.g3@gmail.com>
2020-05-08 13:17:24 +0530

    Presentational and Container Components

--- confusion/src/App.js
+++ confusion/src/App.js
@@ -1,20 +1,16 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
-import {Navbar, NavbarBrand} from 'reactstrap';
-import Menu from './components/MenuComponent';
+import Main from './components/MainComponent';
 
-function App() {
+class App extends Component{
+
+render(){
   return (
     <div>
-      <Navbar dark color="primary">
-      <div className="container">
-      <NavbarBrand href='/'>Ristorante Con Fusion</NavbarBrand>
-      </div>
-      </Navbar>
-      <Menu></Menu>
+      <Main/>
     </div>
   );
+}
 }
 
 export default App;
