c21218113dd29a0425239681d5028b332f043b5b
GouthamSurendran <gouthu.g3@gmail.com>
2020-05-09 13:58:07 +0530

    React Router

--- confusion/src/components/MainComponent.js
+++ confusion/src/components/MainComponent.js
@@ -1,9 +1,11 @@
 import React, { Component } from 'react';
+import Home from './HomeComponent';
 import Menu from './MenuComponent';
 import DishDetail from './DishdetailComponent';
 import {DISHES} from '../shared/dishes';
 import Header from './HeaderComponent';
 import Footer from './FooterComponent';
+import {Switch,Route,Redirect} from 'react-router-dom';
 
 class Main extends Component{
 
@@ -12,22 +14,25 @@ class Main extends Component{
 
     this.state={
       dishes:DISHES,
-      selectedDish:null
     };
   }
-  onDishSelect(dishId) {
-    this.setState({ selectedDish: dishId });
-
-}
-
+ 
 render(){
+
+    const Homepage = () =>{
+        return(
+            <Home/>
+        );
+    }
+
   return (
     <div>
     <Header/>
-      <Menu dishes={this.state.dishes} 
-      onClick={(dishId) => this.onDishSelect(dishId)} />
-      <DishDetail 
-      selectedDish={this.state.dishes.filter((dish)=>dish.id === this.state.selectedDish)[0]}/>
+    <Switch>
+    <Route path='/home' component={Homepage} />
+    <Route exact path='/menu' component={()=> <Menu dishes={this.state.dishes}/>} />}/> //one way to define it.
+    <Redirect to='/home'/>
+    </Switch>
       <Footer/>
     </div>
    
