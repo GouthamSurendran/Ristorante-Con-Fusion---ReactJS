aa564e4543d6a4a8209e1fc680f50e05c92a7413
GouthamSurendran <gouthu.g3@gmail.com>
2020-05-25 14:43:59 +0530

    Transition animation 2

--- confusion/src/components/DishdetailComponent.js
+++ confusion/src/components/DishdetailComponent.js
@@ -4,6 +4,7 @@ import { Link } from 'react-router-dom';
 import { Control, LocalForm, Errors } from 'react-redux-form';
 import { Loading } from './LoadingComponent';
 import { baseUrl } from '../shared/baseUrl';
+import { FadeTransform, Fade, Stagger } from 'react-animation-components';
 
 
 const maxLength = (len) => (val) => !(val) || (val.length <= len);
@@ -98,15 +99,20 @@ class CommentForm extends Component {
 function RenderDish({ dish }) {
 
     if (dish != null) {
-        return (
-            <Card>
-                <CardImg width='100%' src={baseUrl + dish.image} alt={dish.name} />
-                <CardBody>
-                    <CardTitle>{dish.name}</CardTitle>
-                    <CardText>{dish.description}
-                    </CardText>
-                </CardBody>
-            </Card>
+        return ( 
+            <FadeTransform in
+                transformProps={{
+                    exitTransform: 'scale(0.5) translateY(-50%)'
+                }}>
+                <Card>
+                    <CardImg width='100%' src={baseUrl + dish.image} alt={dish.name} />
+                    <CardBody>
+                        <CardTitle>{dish.name}</CardTitle>
+                        <CardText>{dish.description}
+                        </CardText>
+                    </CardBody>
+                </Card>
+            </FadeTransform>
         );
     }
     else {
@@ -117,17 +123,19 @@ function RenderDish({ dish }) {
 function RenderComments({ comments, postComment, dishId }) {
     var commentList = comments.map(comment => {
         return (
+            <Fade in>
             <li key={comment.id}>
                 <p>{comment.comment}</p>
 
                 <p>--{comment.author}, {new Intl.DateTimeFormat('en-US', { year: 'numeric', month: 'short', day: '2-digit' }).format(new Date(Date.parse(comment.date)))}</p>
             </li>
+            </Fade>
         );
     })
     return (
         <div>
             <h4>Comments</h4>
-            <ul className='list-unstyled'>{commentList}</ul>
+            <ul className='list-unstyled'><Stagger in>{commentList}</Stagger></ul>
             <CommentForm dishId={dishId} postComment={postComment} />
         </div>
     );
